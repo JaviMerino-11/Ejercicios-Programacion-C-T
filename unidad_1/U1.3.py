9886b6de1f6d3cf6043fe5866d63b78dce265430
JaviMerino11 <javiermerino10@gmail.com>
2022-10-12 19:55:44 +0200

    [[feat]] exercise number 3 implemented

---
+++ unidad_1/U1.3.py
@@ -0,0 +1,20 @@
+# Escribe código para, dado un entero, generar una
+# cadena con los dígitos incluidos al revés y separados por
+# espacios en blanco (345636 → '6 3 6 5 4 3')
+
+def voltear_entero(numero):
+    numero = str(numero)
+    numero_volteado = numero[::-1]
+    numero_volteado = (int(numero_volteado))
+
+    return numero_volteado
+
+
+def main():
+    numero_pensado_usuario = int(input("Introduzca un número entero con varias cifras: "))
+    print("El numero pensado es %i.\nEl numero volteado es %i." % (
+        numero_pensado_usuario, voltear_entero(numero=numero_pensado_usuario)))
+
+
+if __name__ == '__main__':
+    main()

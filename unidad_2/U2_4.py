26c9ac1ea1ba00c3c0bf481b114f0bb495e9a9bc
JaviMerino11 <javiermerino10@gmail.com>
2022-11-01 21:03:07 +0100

    [[feat]] eratostenes criba implemented

--- unidad_2/U2_4.py
+++ unidad_2/U2_4.py
@@ -1,19 +1,23 @@
 # Escribe un guión en el que se introduzca por la entrada estándar un número, n, se cree una
 # lista con los enteros de 1 a n y genere otra con los primos mediante la criba de Eratóstenes.
 
-# Para ello, el usuario va a introducir un numero entero cualquiera, creamos una lista que vaya desde 1 hasta ese mismo
-# numero introducido. Por otro lado, vamos a crear otra lista que recoja los numeros primos desde 1 hasta n
+# Para ello, el usuario va a introducir un numero entero cualquiera, aplicamos la criba de eratostenes: introducimos
+# los numeros naturales entre 2 y el numero que introduzca el usuario, y voy quitando los numeros que no son primos --
+# cuando se encuentra un número entero que no ha sido tachado, ese número es declarado primo, y se procede a tachar
+# todos sus múltiplos. El proceso termina cuando el cuadrado del mayor número confirmado como primo es mayor que el
+# numero introducido.
+
+def criba_eratostenes(numero_entero: int):
+    multiplos = set()
+    for numero in range(2, numero_entero + 1):
+        if numero not in multiplos:
+            print("Los numeros primos son: ", numero)
+            multiplos.update(range(numero * numero, numero_entero + 1, numero))
+
 
 def main():
     numero_introducido = int(input("Introduzca un numero entero cualquiera: "))
-
-    lista_enteros = list()
-    lista_primos = list()
-
-    for numero in range(1, numero_introducido + 1):  # REVISAR
-        lista_enteros.append(numero)
-        if numero % numero != 0:
-            lista_primos.append(numero)
+    criba_eratostenes(numero_entero=numero_introducido)
 
 
 if __name__ == '__main__':
